a1707df708544bd1bbc74fd651af4f739a5a5d9f
Molnar Dávid <molnardavid2002@gmail.com>
2023-08-21 15:47:22 +0200

    QuickEdit option progress

--- screens/QuickEdit.tsx
+++ screens/QuickEdit.tsx
@@ -1,12 +1,51 @@
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  Pressable,
+  StyleSheet,
+  TouchableHighlight,
+} from "react-native";
 
 type QuickEditProps = {};
+
+const handleMembershipButton = () => {
+  console.log("levont");
+};
+
 function QuickEdit(params: QuickEditProps) {
   return (
-    <View>
-      <Text> QUCK EDIT </Text>
+    <View style={styles.container}>
+      <Text>USER NAME </Text>
+      <View>
+        <TouchableHighlight
+          style={styles.button}
+          onPress={handleMembershipButton}
+        >
+          <Text style={styles.buttonText}> 10 </Text>
+        </TouchableHighlight>
+      </View>
     </View>
   );
 }
 
 export default QuickEdit;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+  button: {
+    backgroundColor: "blue",
+    borderRadius: 12,
+    color: "white",
+  },
+  buttonText: {
+    fontSize: 124,
+    color: "white",
+    width: "100%",
+    borderRadius: 24,
+  },
+});
