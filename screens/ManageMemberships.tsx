d0f30543bf94bf17fd705d13c5c60d4d29007686
mdave94 <molnardavid2002@gmail.com>
2023-09-18 01:17:32 +0200

    ManageMembeship page defautl design is added

--- screens/ManageMemberships.tsx
+++ screens/ManageMemberships.tsx
@@ -1,13 +1,54 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
+import CustomButton from "../components/UI/CustomButton";
+import MembershipTypeCard from "../components/UI/MembershipTypeCard";
 
 type ManageMembershipsProps = {};
 
 function ManageMemberships(params: ManageMembershipsProps) {
+  function addMembershipHandler() {}
+
   return (
-    <View>
-      <Text> ManageMemberships </Text>
+    <View style={styles.container}>
+      <View style={styles.button}></View>
+      <CustomButton>Bérlettípus Hozzáada</CustomButton>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.cardContainer}>
+          <MembershipTypeCard />
+        </View>
+        <View style={styles.cardContainer}>
+          <MembershipTypeCard />
+        </View>
+        <View style={styles.cardContainer}>
+          <MembershipTypeCard />
+        </View>
+        <View style={styles.cardContainer}>
+          <MembershipTypeCard />
+        </View>
+        <View style={styles.cardContainer}>
+          <MembershipTypeCard />
+        </View>
+        <View style={styles.cardContainer}>
+          <MembershipTypeCard />
+        </View>
+        <View style={styles.cardContainer}>
+          <MembershipTypeCard />
+        </View>
+      </ScrollView>
     </View>
   );
 }
 
 export default ManageMemberships;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  cardContainer: {
+    marginVertical: 10,
+  },
+  button: {
+    marginTop: 32,
+  },
+});
